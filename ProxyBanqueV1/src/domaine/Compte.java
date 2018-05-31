cca336474a992a2fab7144b9529fb3c14d371752
quentin ussunet <qussunet@gmail.com>
2018-05-31 15:07:10 +0200

    test erreur

--- ProxyBanqueV1/src/domaine/Compte.java
+++ ProxyBanqueV1/src/domaine/Compte.java
@@ -2,8 +2,38 @@ package domaine;
 
 public class Compte {
 	
+	//---------- Attributs de la Classe Adresse 
+	
 	protected int numeroDeCompte;
 	protected double solde=0.;
 	protected String dateOuvertureCompte;
+	
+	
+	//---------- Constructeurs de la Classe Adresse
+	
+	
+	
+	//---------- Getters & Setters
 
+	public int getNumeroDeCompte() {
+		return numeroDeCompte;
+	}
+	public void setNumeroDeCompte(int numeroDeCompte) {
+		this.numeroDeCompte = numeroDeCompte;
+	}
+	public double getSolde() {
+		return solde;
+	}
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+	public String getDateOuvertureCompte() {
+		return dateOuvertureCompte;
+	}
+	public void setDateOuvertureCompte(String dateOuvertureCompte) {
+		this.dateOuvertureCompte = dateOuvertureCompte;
+	}
+	
+	//---------- Methode toString
+	
 }
